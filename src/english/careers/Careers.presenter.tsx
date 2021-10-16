fa90ca3c0fffc6d0230d61a04ab0a31f76cfafee
TaewoongMoon <taewoongmoon1@gmail.com>
2021-10-16 19:31:23 +0900

    Fix: Pre-rendering에러 from careers page 고침

--- src/english/careers/Careers.presenter.tsx
+++ src/english/careers/Careers.presenter.tsx
@@ -26,53 +26,61 @@ interface IProps {
   language: any
 }
 
-const CareersUI = ({ data }: IProps) => {
+const CareersUI = ({ data, language }: IProps) => {
   const router = useRouter()
   const pageIndex = Number(router.query.item)
-  console.log(router.query)
+  console.log('pageIndex', pageIndex)
   return (
     <>
-      {data.careers[pageIndex]?.layout === '인재상페이지' && (
-        <TalentsWrapper>
-          <TalentTitle>{data.careers[pageIndex].name}</TalentTitle>
-          <TalentLineDivider />
-          {data.careers[pageIndex].circles.map((data: any) => (
-            <BigCircleWrapper key="">
-              <Circle>
-                <CircleText>{data.title}</CircleText>
-              </Circle>
-              <CircleDetailText>{data.content}</CircleDetailText>
-            </BigCircleWrapper>
-          ))}
-        </TalentsWrapper>
-      )}
-      {data.careers[pageIndex]?.layout === '인사제도페이지' && (
-        <TalentsWrapper>
-          <HrTitle>{data.careers[pageIndex].name}</HrTitle>
-          <HrLineDivider />
-          <SubTitleWrapper>{data.careers[pageIndex].subTitle}</SubTitleWrapper>
-          <HrCirclesWrapper>
-            <HrCircleFirst>{data.careers[pageIndex].circles[0]}</HrCircleFirst>
-            <CareersArrow data={data.careers[pageIndex].images[0]} />
-            <HrCircleSecond>
-              {data.careers[pageIndex].circles[1]}
-            </HrCircleSecond>
-            <CareersArrow data={data.careers[pageIndex].images[0]} />
-            <HrCircleThird>{data.careers[pageIndex].circles[2]}</HrCircleThird>
-          </HrCirclesWrapper>
-          <HrPrincipleTitle>
-            {data.careers[pageIndex].secondTitle}
-          </HrPrincipleTitle>
-          <HrLineDivider />
-          <HrPrincipleDetailsBigWrapper>
-            {data.careers[pageIndex].principle.map((data: any) => (
-              <HrPrincipleDetailsWrapper key="">
-                {data}
-              </HrPrincipleDetailsWrapper>
+      {language === 'kor' &&
+        data.careers[pageIndex]?.layout === '인재상페이지' && (
+          <TalentsWrapper>
+            <TalentTitle>{data.careers[pageIndex]?.name}</TalentTitle>
+            <TalentLineDivider />
+            {data.careers[pageIndex]?.circles.map((data: any) => (
+              <BigCircleWrapper key="">
+                <Circle>
+                  <CircleText>{data.title}</CircleText>
+                </Circle>
+                <CircleDetailText>{data.content}</CircleDetailText>
+              </BigCircleWrapper>
             ))}
-          </HrPrincipleDetailsBigWrapper>
-        </TalentsWrapper>
-      )}
+          </TalentsWrapper>
+        )}
+      {language === 'kor' &&
+        data.careers[pageIndex]?.layout === '인사제도페이지' && (
+          <TalentsWrapper>
+            <HrTitle>{data.careers[pageIndex].name}</HrTitle>
+            <HrLineDivider />
+            <SubTitleWrapper>
+              {data.careers[pageIndex].subTitle}
+            </SubTitleWrapper>
+            <HrCirclesWrapper>
+              <HrCircleFirst>
+                {data.careers[pageIndex].circles[0]}
+              </HrCircleFirst>
+              <CareersArrow data={data.careers[pageIndex].images[0]} />
+              <HrCircleSecond>
+                {data.careers[pageIndex].circles[1]}
+              </HrCircleSecond>
+              <CareersArrow data={data.careers[pageIndex].images[0]} />
+              <HrCircleThird>
+                {data.careers[pageIndex].circles[2]}
+              </HrCircleThird>
+            </HrCirclesWrapper>
+            <HrPrincipleTitle>
+              {data.careers[pageIndex].secondTitle}
+            </HrPrincipleTitle>
+            <HrLineDivider />
+            <HrPrincipleDetailsBigWrapper>
+              {data.careers[pageIndex].principle.map((data: any) => (
+                <HrPrincipleDetailsWrapper key="">
+                  {data}
+                </HrPrincipleDetailsWrapper>
+              ))}
+            </HrPrincipleDetailsBigWrapper>
+          </TalentsWrapper>
+        )}
     </>
   )
 }
