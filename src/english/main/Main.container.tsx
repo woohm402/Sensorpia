0f986a1f4e88aeb013baedbd9dda29de06346ba6
TaewoongMoon <taewoongmoon1@gmail.com>
2021-10-17 05:46:36 +0900

    Add: useEffect훅 안에 setInterval함수를 사용하여 5초마다 슬라이더 자동으로 넘어가게끔 구현, 버튼을 누르면 넘어간 화면에서 5초대기 후 다음화면으로 넘어감

--- src/english/main/Main.container.tsx
+++ src/english/main/Main.container.tsx
@@ -1,14 +1,33 @@
 import { Router, useRouter } from 'next/dist/client/router'
-import { useContext, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { GlobalContext } from '../../../pages/_app'
 import MainUI from './Main.presenter'
+const timer = {}
 
 const MainComponent = () => {
   const { language } = useContext(GlobalContext)
   const data = require(`../../../pages/api/${language}.json`)
   const [translate, setTranslate] = useState(0)
   const router = useRouter()
+  const setIntervalFunction = () => {}
+  useEffect(() => {
+    //@ts-ignore
+    timer.temp = setInterval(function () {
+      if (translate === -3300) {
+        setTranslate(0)
+        //@ts-ignore
+        clearInterval(timer.temp)
+      } else {
+        setTranslate((prev) => prev - 1100)
+        //@ts-ignore
+        clearInterval(timer.temp)
+      }
+    }, 5000)
+  }, [translate])
+
   const onClickSliderNext = () => {
+    //@ts-ignore
+    clearInterval(timer.temp)
     if (translate === -3300) {
       setTranslate(0)
     } else {
@@ -16,6 +35,8 @@ const MainComponent = () => {
     }
   }
   const onClickSliderPrevious = () => {
+    //@ts-ignore
+    clearInterval(timer.temp)
     if (translate === 0) return
     setTranslate((prev) => prev + 1100)
   }
