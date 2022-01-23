8233951347d16f05113c0fda4dc31606d8519fde
woohm402 <woohm404@gmail.com>
2022-01-23 20:54:02 +0900

    로그인 기능 구현

--- pages/api/admin/login.ts
+++ pages/api/admin/login.ts
@@ -1,6 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { withSessionRoute } from '../../../src/lib/withSession'
+import bcrypt from 'bcrypt'
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default withSessionRoute(async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const { username, password } = req.body
 
   if (!username) {
@@ -13,5 +18,28 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     return
   }
 
-  res.status(200).json({ name: 'John Doe' })
-}
+  if (!process.env.USERNAME || !process.env.PASSWORD) {
+    res.status(500)
+  }
+
+  const isCorrectUsername = process.env.USERNAME === username
+  const isCorrectPassword = await bcrypt.compare(
+    password,
+    '$2a$07$' + process.env.PASSWORD!
+  )
+
+  if (!isCorrectPassword || !isCorrectUsername) {
+    res
+      .status(401)
+      .json({ message: '사용자 이름이나 비밀번호가 올바르지 않습니다.' })
+    return
+  }
+
+  // @ts-ignore
+  req.session.user = {
+    admin: true,
+  }
+
+  await req.session.save()
+  res.status(200).json({ success: true })
+})
