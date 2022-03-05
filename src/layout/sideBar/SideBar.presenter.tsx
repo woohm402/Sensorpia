5b1cfa6dc0db528200187bf8b7e5d71181ddad1c
woohm402 <woohm404@gmail.com>
2022-03-05 18:00:47 +0900

    페이지 추가 기능 구현

--- src/layout/sideBar/SideBar.presenter.tsx
+++ src/layout/sideBar/SideBar.presenter.tsx
@@ -14,6 +14,7 @@ import {
 } from './SideBar.styles';
 import { useLanguageContext } from '../../context/language/language';
 import { Fragment } from 'react';
+import { replaceValue } from '../../lib/replaceValue';
 
 interface IProps {
   onClickMenu: any;
@@ -23,7 +24,11 @@ interface IProps {
 const SideBarUI = ({ onClickMenu, buttonName }: IProps) => {
   const router = useRouter();
   const pageIndex = Number(router.query.item);
-  const { language, languageData: data } = useLanguageContext();
+  const {
+    language,
+    languageData: data,
+    setCurrentLanguageData,
+  } = useLanguageContext();
   const onClickScrollArrow = () => {
     window.scrollTo(0, 0);
   };
@@ -40,18 +45,18 @@ const SideBarUI = ({ onClickMenu, buttonName }: IProps) => {
           : data.sidebar.name[2]}
       </SideBarTitleWrapper>
       <SideBarMenusWrapper>
-        {router.pathname === '/products'
-          ? data.products.map((value: any, i: number) => (
+        {router.pathname === '/products' ? (
+          <>
+            {data.products.map((value, i) => (
               <Fragment key={i}>
                 <SideBarMenusInnerWrapper
                   id={`${i}`}
                   onClick={onClickMenu}
-                  //@ts-ignore
                   $color={pageIndex == i}
                 >
                   {value.name}
                 </SideBarMenusInnerWrapper>
-                {value.name === '3-axis flux-gate magnetometer' && (
+                {value.data.subSections.length > 1 && (
                   <SideBarProductsSubMenusWrapper>
                     {value.data.subSections.map((data: any, i: number) => (
                       <SideBarProductsIndividualWrapper key={i}>
@@ -61,7 +66,6 @@ const SideBarUI = ({ onClickMenu, buttonName }: IProps) => {
                         <SideBarProductsIndividualText
                           id={data.name}
                           onClick={onClickMenu}
-                          //@ts-ignore
                           $color={data.name === buttonName}
                         >
                           {data.name}
@@ -70,57 +74,76 @@ const SideBarUI = ({ onClickMenu, buttonName }: IProps) => {
                     ))}
                   </SideBarProductsSubMenusWrapper>
                 )}
+                <button style={{ margin: '8px 0' }} onClick={() => {}}>
+                  하위 페이지 추가
+                </button>
                 {data.products.length - 1 !== i && <SideBarMenusLineDivider />}
               </Fragment>
-            ))
-          : router.pathname === '/application'
-          ? data.applications.map((value: any, i: number) => (
-              <Fragment key={i}>
-                <SideBarMenusInnerWrapper
-                  id={`${i}`}
-                  onClick={onClickMenu}
-                  //@ts-ignore
-                  $color={pageIndex === i}
-                >
-                  {value.name}
-                </SideBarMenusInnerWrapper>
-                {data.applications.length - 1 !== i && (
-                  <SideBarMenusLineDivider />
-                )}
-              </Fragment>
-            ))
-          : router.pathname === '/careers' && language === 'kor'
-          ? data.careers?.map((value, i, arr) => (
-              <Fragment key={i}>
-                <SideBarMenusInnerWrapper
-                  id={`${i}`}
-                  onClick={onClickMenu}
-                  //@ts-ignore
-                  $color={pageIndex === i}
-                >
-                  {value.name}
-                </SideBarMenusInnerWrapper>
-                {arr.length - 1 !== i && <SideBarMenusLineDivider />}
-              </Fragment>
-            ))
-          : data.aboutUs.map((value, i) => (
-              <Fragment key={i}>
-                <SideBarMenusInnerWrapper
-                  id={`${i}`}
-                  onClick={onClickMenu}
-                  //@ts-ignore
-                  $color={pageIndex === i}
-                >
-                  {value.name}
-                </SideBarMenusInnerWrapper>
-                {data.aboutUs.length - 1 !== i && <SideBarMenusLineDivider />}
-              </Fragment>
             ))}
+            <button
+              style={{ width: '100%', marginTop: 16 }}
+              onClick={() => {
+                setCurrentLanguageData(
+                  replaceValue(
+                    data,
+                    'products',
+                    data?.products.concat(
+                      data.products[data.products.length - 1]
+                    )
+                  )
+                );
+              }}
+            >
+              페이지 추가
+            </button>
+          </>
+        ) : router.pathname === '/application' ? (
+          data.applications.map((value, i) => (
+            <Fragment key={i}>
+              <SideBarMenusInnerWrapper
+                id={`${i}`}
+                onClick={onClickMenu}
+                $color={pageIndex === i}
+              >
+                {value.name}
+              </SideBarMenusInnerWrapper>
+              {data.applications.length - 1 !== i && (
+                <SideBarMenusLineDivider />
+              )}
+            </Fragment>
+          ))
+        ) : router.pathname === '/careers' && language === 'kor' ? (
+          data.careers?.map((value, i, arr) => (
+            <Fragment key={i}>
+              <SideBarMenusInnerWrapper
+                id={`${i}`}
+                onClick={onClickMenu}
+                //@ts-ignore
+                $color={pageIndex === i}
+              >
+                {value.name}
+              </SideBarMenusInnerWrapper>
+              {arr.length - 1 !== i && <SideBarMenusLineDivider />}
+            </Fragment>
+          ))
+        ) : (
+          data.aboutUs.map((value, i) => (
+            <Fragment key={i}>
+              <SideBarMenusInnerWrapper
+                id={`${i}`}
+                onClick={onClickMenu}
+                $color={pageIndex === i}
+              >
+                {value.name}
+              </SideBarMenusInnerWrapper>
+              {data.aboutUs.length - 1 !== i && <SideBarMenusLineDivider />}
+            </Fragment>
+          ))
+        )}
       </SideBarMenusWrapper>
       {router.pathname === '/aboutUs' && (
         <ScrollArrowImageWrapper
           onClick={onClickScrollArrow}
-          //@ts-ignore
           data={data.aboutUs[pageIndex]?.commonArrow}
         />
       )}
