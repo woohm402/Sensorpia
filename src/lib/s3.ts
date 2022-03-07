2cd2c1f78f1ad22fec078236d7a9d4043a251f39
woohm402 <woohm404@gmail.com>
2022-03-07 20:41:47 +0900

    fix env vars

--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -1,7 +1,13 @@
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 
-const client = new S3Client({});
+const client = new S3Client({
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID_SENSORPIA ?? '',
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_SENSORPIA ?? '',
+  },
+  region: process.env.AWS_DEFAULT_REGION_SENSORPIA ?? '',
+});
 
 export const putJSON = (key: string, body: string) => {
   return client.send(
